7dfc336314d85c5c68415c0181f2e531c98f42c6
alexandrularion <larion.a@yahoo.com>
2025-09-10 20:23:55 +0300

    Updated concepts for JavaScriptRO48

--- JavaScriptRO48/jest-matchers/src/index.js
+++ JavaScriptRO48/jest-matchers/src/index.js
@@ -1,22 +1,24 @@
 // String
 
 function bankGreeting(userName) {
+  if (!userName) {
+    throw Error("userName parameter is missing");
+  }
+
   const currentDate = new Date(
     new Date().toLocaleString(undefined, {
       hour12: false,
     })
   );
 
-  if (currentDate.getHours() > 18) {
+  if (currentDate.getHours() >= 18) {
     return `Good evening, ${userName}`;
-  } else if (currentDate.getDate() > 11) {
+  } else if (currentDate.getHours() >= 11) {
     return `Good afternoon, ${userName}`;
   } else {
     return `Good morning, ${userName}`;
   }
 }
-
-console.log(bankGreeting("Barosan"));
 
 // Number
 
--- JavaScriptRO48/jest-matchers/src/index.test.js
+++ JavaScriptRO48/jest-matchers/src/index.test.js
@@ -1,28 +1,47 @@
 const { bankGreeting } = require("./index"); // Common JS Import
 
 describe("when calling bankGreeting function", () => {
+  // Inainte de fiecare test
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
   // Testele noastre
-  // beforeAll(() => {
-  //   const mockData = new Date(0, 0, 0, 19, 30, 0);
-  //   jest.spyOn(global, "Date").mockImplementation(() => mockData);
-  // });
-
-  // afterAll(() => {
-  //   jest.restoreAllMocks();
-  // });
-
   test("should return greeting if userName parameter was passed as string", () => {
     const mockData = new Date(0, 0, 0, 19, 30, 0);
     jest.spyOn(global, "Date").mockImplementation(() => mockData);
 
     const greeting = bankGreeting("Barosane");
 
+    console.debug(greeting); // "Good evening, Barosane"
+
     expect(greeting).toBe("Good evening, Barosane");
   });
 
-  test("...", () => {
-    //
+  test("should return morning greeting if hour is between 00:00 -> 10:59", () => {
+    const mockData = new Date(0, 0, 0, 8, 0, 0);
+    jest.spyOn(global, "Date").mockImplementation(() => mockData);
+
+    const greeting = bankGreeting("John");
+
+    console.debug(greeting); // "Good morning, John"
+
+    expect(greeting).toBe("Good morning, John");
+  });
+
+  test("should return afternoon greeting if hour is between 11:00 -> 17:59", () => {
+    const mockData = new Date(0, 0, 0, 14, 29, 0);
+    jest.spyOn(global, "Date").mockImplementation(() => mockData);
+
+    const greeting = bankGreeting("Ion");
+
+    console.debug(greeting); // "Good afternoon, Ion"
+
+    expect(greeting).toBe("Good afternoon, Ion");
+  });
+
+  test("should throw an error if userName parameter is missing", () => {
+    expect(() => bankGreeting()).toThrow();
+    expect(() => bankGreeting()).toThrow("userName parameter is missing");
   });
 });
-
-//
